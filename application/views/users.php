e984c7cf579c5c3099c0d740e30f6b7ab7526ea6
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-06 15:42:02 +0530

    translate

--- application/views/users.php
+++ application/views/users.php
@@ -53,6 +53,7 @@
                                 <th>Email</th>
                                 <th>Phone Number</th>
                                 <th>Authority</th>
+								<th>Created On</th>
                                 <th>Transactions</th>
                             </tr>
                   </thead>
@@ -79,6 +80,7 @@
                                     <td>
                                         <?php echo $record->role ?>
                                     </td>
+									<td><?php echo 'date("d-m-Y", strtotime($record->createdDtm))' ?></td>
                                     <td class="text-center">
                                         <a class="btn btn-sm btn-primary" href="<?= base_url().'log-history/'.$record->userId; ?>" title="Log history">
                                             <i class="fa fa-history"></i>
