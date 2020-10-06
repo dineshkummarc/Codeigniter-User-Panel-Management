486158215e3992ef3ad2efdd7b0f2cd942027d5c
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-06 23:40:18 +0530

    translate

--- application/views/404.php
+++ application/views/404.php
@@ -2,7 +2,7 @@
     <section class="content-header">
         <h1>
             404
-            <small>Aradığınız sayfa maalasef bulunamadı.</small>
+            <small>The page you are looking for was not found maalasef.</small>
         </h1>
     </section>
     <section class="content">
