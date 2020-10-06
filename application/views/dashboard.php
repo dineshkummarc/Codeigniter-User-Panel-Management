62185b20336905f165ad8fb57d160aa3282bebe6
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-06 12:36:09 +0530

    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -44,6 +44,25 @@
         </div>
       </div>
       <!-- ./col -->
+	  
+	  <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+          <div class="inner">
+            <h3>
+              <?php if(isset($finishedTasksCount)) { echo $finishedTasksCount; } else { echo '0'; } ?>%
+            </h3>
+            <p>Completed Tasks</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-pie-graph"></i>
+          </div>
+          <a href="<?php echo base_url(); ?><?php  if($role != ROLE_EMPLOYEE) {echo 'tasks';}else{echo 'etasks';} ?>" class="small-box-footer">More information
+            <i class="fa fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+	  
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-yellow">
