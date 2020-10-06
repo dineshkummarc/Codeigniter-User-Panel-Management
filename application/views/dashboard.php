0ab7e65ab88bf635a24ebd234e7077fcb32483c8
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-07 01:50:22 +0530

    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -26,6 +26,21 @@
         </div>
       </div>
       <!-- ./col -->
+	  
+	  <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-blue">
+                <div class="inner">
+                  <h3>65</h3>
+                  <p>Open tasks</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More information <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+	  
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-green">
@@ -106,22 +121,7 @@
         </div>
       </div>
       <!-- ./col -->
-	  
-	  <div class="col-lg-3 col-xs-6">
-              <!-- small box -->
-              <div class="small-box bg-blue">
-                <div class="inner">
-                  <h3>65</h3>
-                  <p>Reopened tasks</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-pie-graph"></i>
-                </div>
-                <a href="#" class="small-box-footer">More information <i class="fa fa-arrow-circle-right"></i></a>
-              </div>
-            </div><!-- ./col -->
-          </div>
-	  
+     </div>
     </div>
   </section>
 </div>
